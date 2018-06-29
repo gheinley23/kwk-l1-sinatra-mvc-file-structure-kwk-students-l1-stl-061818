35e4c225322302f4a8dcda698291bbcca7fd9e9b
gheinley23 <gheinley23@jacksonindians.us>
2018-06-29 16:21:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require './config/environment'
+require './app/models/model'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -5,7 +7,19 @@ class ApplicationController < Sinatra::Base
   	set :public_dir, "public"
   end
 
-  get "/" do
-  	erb :index
+  get '/' do    #this is your default/homepage
+    return erb :index  
+#   get "/newpage"do
+#     erb :newpage
   end
+  
+  # get '/get_name'do
+  #   first_dog = Dog.new("rudolph","mastiff",2)
+  #   first_dog.get_name + first_dog.get_breed + first_dog.get_age
+     post '/' do  #we click the button now what?
+       @the_fortune=get_fortune #method get_fortune is connected to the instance variable @the_fortune
+       @the_user=params[:user] #creating a new instance variable the_user
+       #hash that stores user input ex: => value
+       return erb :results
+ end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,10 @@
-class Model
-  # Replace with Dog clas
+def get_fortune
+  
+  fortune_array=["you will drink 20 core water bottles this week","you will win the next vegtaible off","Your lunch will be to die for (really good)!"]
+# fortune = fortune_array.sample
+#   fortune_array.delete(fortune)
+#   return fortune
+return fortune_array.sample
 end
+
+ 
